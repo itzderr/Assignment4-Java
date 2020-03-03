432a0b8beea1ab6a9e7e44f62b190c7771dcad11
Tomona Sako <sakotomona@gmail.com>
2020-03-03 10:16:19 -0800

    finish Assignment4 by Tomona

--- src/main/java/ca/ciccc/SeatingChart.java
+++ src/main/java/ca/ciccc/SeatingChart.java
@@ -13,24 +13,38 @@ public class SeatingChart {
      * Creates a seating chart with the given number of rows and columns from the
      * students in {@code studentList}. Empty seats in the seating chart are represented
      * by {@code null}.
+     *
      * @param studentList the list of students.
-     * @param rows the number of rows of seats in the classroom.
-     * @param cols the number of columns of seats in the classroom.
-     *
-     * Pre-condition: rows > 0; cols > 0;
-     *               rows * cols >= studentList.size();
-     *
-     * Post-condition:
-     *             - Students appear in the seating chart in the same order as they appear
-     *               in {@code studentList}, starting at {@code seats[0][0]}.
-     *             - {@code seats} is filled column by column from {@code studentList}, followed by
-     *               any empty seats (represented by {@code null}.
-     *             - {@code studentList} is unchanged.
+     * @param rows        the number of rows of seats in the classroom.
+     * @param cols        the number of columns of seats in the classroom.
+     *                    <p>
+     *                    Pre-condition: rows > 0; cols > 0;
+     *                    rows * cols >= studentList.size();
+     *                    <p>
+     *                    Post-condition:
+     *                    - Students appear in the seating chart in the same order as they appear
+     *                    in {@code studentList}, starting at {@code seats[0][0]}.
+     *                    - {@code seats} is filled column by column from {@code studentList}, followed by
+     *                    any empty seats (represented by {@code null}.
+     *                    - {@code studentList} is unchanged.
+     *                    <p>
+     *                    list 3 4
      */
     public SeatingChart(List<Student> studentList, int rows, int cols) {
         // TODO: Your code goes here.
-
+        seats = new Student[rows][cols];
+        int counter = 0;
+        for (int c = 0; c < cols; c++) {
+            for (int r = 0; r < rows; r++) {
+                seats[r][c] = studentList.get(counter);
+                counter++;
+                if (counter == studentList.size()) {
+                    break;
+                }
+            }
+        }
     }
+
 
     /**
      * Removes students who have more than a given number of absences from the seating chart,
@@ -39,24 +53,32 @@ public class SeatingChart {
      *
      * @param allowedAbsences an integer >= 0
      * @return the number of students removed from {@code seats}.
-     *
+     * <p>
      * Pre-condition:
-     *   - All students with {@code allowedAbsences} or fewer are in their original positions in
-     *     {@code seats}.
-     *   - No student in {@code seats} has more than {@code allowedAbsences} absences.
-     *   - Entries without students contain {@code null}.
+     * - All students with {@code allowedAbsences} or fewer are in their original positions in
+     * {@code seats}.
+     * - No student in {@code seats} has more than {@code allowedAbsences} absences.
+     * - Entries without students contain {@code null}.
      */
     public int removeAbsentStudents(int allowedAbsences) {
         // TODO: your code goes here.
-
-        return 0;
+        int counter = 0;
+        for (Student[] person : seats) {
+            for (int r = 0; r < seats.length; r++) {
+                if (person[r].getAbsenceCount() > allowedAbsences) {
+                    person[r] = null;
+                    counter++;
+                }
+            }
+        }
+        return counter;
     }
 
     @Override
     public String toString() {
         String result = "";
-        for(Student[] row: seats) {
-            for(Student col: row) {
+        for (Student[] row : seats) {
+            for (Student col : row) {
                 result += (col != null) ? col.toString() + " " : "null ";
             }
             result += "\n";
