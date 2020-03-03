e49638c0a2ddfd0eda04e36defaee4d77ddf6a37
Cayo <cayomesquita@gmail.com>
2020-03-03 12:08:55 -0800

    Resolve Assignment4

--- src/main/java/ca/ciccc/SeatingChart.java
+++ src/main/java/ca/ciccc/SeatingChart.java
@@ -13,23 +13,34 @@ public class SeatingChart {
      * Creates a seating chart with the given number of rows and columns from the
      * students in {@code studentList}. Empty seats in the seating chart are represented
      * by {@code null}.
+     *
      * @param studentList the list of students.
-     * @param rows the number of rows of seats in the classroom.
-     * @param cols the number of columns of seats in the classroom.
-     *
-     * Pre-condition: rows > 0; cols > 0;
-     *               rows * cols >= studentList.size();
-     *
-     * Post-condition:
-     *             - Students appear in the seating chart in the same order as they appear
-     *               in {@code studentList}, starting at {@code seats[0][0]}.
-     *             - {@code seats} is filled column by column from {@code studentList}, followed by
-     *               any empty seats (represented by {@code null}.
-     *             - {@code studentList} is unchanged.
+     * @param rows        the number of rows of seats in the classroom.
+     * @param cols        the number of columns of seats in the classroom.
+     *                    <p>
+     *                    Pre-condition: rows > 0; cols > 0;
+     *                    rows * cols >= studentList.size();
+     *                    <p>
+     *                    Post-condition:
+     *                    - Students appear in the seating chart in the same order as they appear
+     *                    in {@code studentList}, starting at {@code seats[0][0]}.
+     *                    - {@code seats} is filled column by column from {@code studentList}, followed by
+     *                    any empty seats (represented by {@code null}.
+     *                    - {@code studentList} is unchanged.
      */
     public SeatingChart(List<Student> studentList, int rows, int cols) {
-        // TODO: Your code goes here.
-
+        if (studentList.size() <= rows * cols) {
+            seats = new Student[rows][cols];
+            int indexRow = 0;
+            int indexCollumn = 0;
+            for (Student student : studentList) {
+                if (indexRow >= rows) {
+                    indexCollumn++;
+                    indexRow = 0;
+                }
+                seats[indexRow++][indexCollumn] = student;
+            }
+        }
     }
 
     /**
@@ -39,24 +50,31 @@ public class SeatingChart {
      *
      * @param allowedAbsences an integer >= 0
      * @return the number of students removed from {@code seats}.
-     *
+     * <p>
      * Pre-condition:
-     *   - All students with {@code allowedAbsences} or fewer are in their original positions in
-     *     {@code seats}.
-     *   - No student in {@code seats} has more than {@code allowedAbsences} absences.
-     *   - Entries without students contain {@code null}.
+     * - All students with {@code allowedAbsences} or fewer are in their original positions in
+     * {@code seats}.
+     * - No student in {@code seats} has more than {@code allowedAbsences} absences.
+     * - Entries without students contain {@code null}.
      */
     public int removeAbsentStudents(int allowedAbsences) {
-        // TODO: your code goes here.
-
-        return 0;
+        int sum = 0;
+        for (int i = 0; i < seats.length; i++) {
+            for (int j = 0; j < seats[i].length; j++) {
+                if (seats[i][j] != null && seats[i][j].getAbsenceCount() > allowedAbsences) {
+                    seats[i][j] = null;
+                    sum++;
+                }
+            }
+        }
+        return sum;
     }
 
     @Override
     public String toString() {
         String result = "";
-        for(Student[] row: seats) {
-            for(Student col: row) {
+        for (Student[] row : seats) {
+            for (Student col : row) {
                 result += (col != null) ? col.toString() + " " : "null ";
             }
             result += "\n";
